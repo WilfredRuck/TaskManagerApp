afece71ead42b54a65e4930d7439d20452ff86eb
Wilfred Ruck <wilfredoruck@gmail.com>
2019-05-13 21:23:40 -0400

    add admin to users table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(
+	{username: "John Doe", email: "johndoe@gmail.com", password: "password123", password_confirmation: "password123", admin: true}
+)
